2ad74c3d288c1b0caa8479f978ff322b7bc13eb1
Ganesh Nalawade <ganesh634@gmail.com>
2024-03-28 23:58:44 +0530

    Update folder structure and yarn lock file (#8)
    Co-authored-by: Ansible-lightspeed-Bot <ansible_lightspeed_bot@redhat.com>

---
+++ ansible/src/components/LearnContent/LearnContent.tsx
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2024 The Ansible plugin Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import { InfoCard } from '@backstage/core-components';
+import { Grid, Typography, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  container: {
+    backgroundColor: 'default',
+    padding: '20px',
+  },
+  text: {
+    color: 'white',
+    marginTop: '5px',
+    fontSize: '15px', // Increase the font size as needed
+  },
+  divider: {
+    margin: '20px 0',
+    backgroundColor: 'white', // Make the divider white so it stands out on the light blue background
+  },
+});
+
+
+ const EntityLearnIntroCard = () => {
+  const classes = useStyles();
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <InfoCard title="Go from zero to hero!">
+          <Typography variant="body1" className={classes.text}>
+            End to end learning journey put together by Red Hat Ansible for any user at any level.<br />
+            If you are a complete beginner to Ansible, this will be the perfect place to start. If you are advanced user,<br />
+            we do recommend going over and catch some stuff you missed have missed otherwise.
+          </Typography>
+        </InfoCard>
+      </Grid>
+    </Grid>
+  )
+}
+
+
+export const EntityLearnContent = () => {
+  return (
+    <EntityLearnIntroCard />
+  );
+};
---
+++ scaffolder-backend-module-ansible/src/actions/run/ansible.ts
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2021 The Ansible plugin Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { createTemplateAction } from '@backstage/plugin-scaffolder-node';
+import { ansibleCreatorRun } from './ansibleContentCreate';
+import { Logger } from 'winston';
+
+export function createAnsibleContentAction() {
+  return createTemplateAction<{
+    repoUrl: string;
+    description: string;
+    collectionGroup: string;
+    collectionName: string;
+  }>({
+    id: 'ansible:content:create',
+    description: 'Runs Ansible creator to scaffold Ansible content',
+    schema: {
+      input: {
+        type: 'object',
+        required: ['repoUrl', 'collectionGroup', 'collectionName'],
+        properties: {
+          repoUrl: {
+            title: 'Repository URL',
+            description: 'The URL of the repository to create the Ansible content',
+            type: 'RepoUrlPicker',
+          },
+          collectionGroup: {
+            title: 'Collection',
+            description: 'The "collectionOrg" part of "collectionOrg.collectionName',
+            type: 'string',
+          },
+          collectionName: {
+            title: 'Collection name',
+            description: 'The "collectionName" part of "collectionOrg.collectionName"',
+            type: 'string',
+          },
+          description: {
+            title: 'Description',
+            description: 'Describe this Collection and its purpose to help other users know what to use it for',
+            type: 'string',
+          },
+        },
+      },
+    },
+    async handler(ctx) {
+      const { repoUrl, description, collectionGroup, collectionName } = ctx.input;
+      ctx.logger.info(
+        `Creating Ansible content within ${collectionGroup}.${collectionName} collection at ${repoUrl} with description: ${description}`
+      );
+      await ansibleCreatorRun(
+        ctx.workspacePath,
+        ctx.logger as Logger,
+        repoUrl,
+        description,
+        collectionGroup,
+        collectionName,
+      );
+    },
+  });
+}
