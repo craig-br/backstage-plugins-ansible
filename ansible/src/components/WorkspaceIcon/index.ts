800dfa367bb61b4aeb981fb6b37764fa1eafe9d0
Ajinkya Udgirkar <ajinkyaudgirkar@gmail.com>
2024-04-02 09:27:10 +0530

    Implemented Overview tab UI

--- ansible/src/components/WorkspaceIcon/index.ts
+++ ansible/src/components/WorkspaceIcon/index.ts
@@ -13,6 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-export { ansiblePlugin, AnsiblePage } from './plugin';
-export { AnsibleLogo } from "./components/AnsibleLogo";
+export { WorkspaceIcon } from './WorkspaceIcon';
