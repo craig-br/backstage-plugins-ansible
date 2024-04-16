bc2dfac32b080cfc27bd87b804360e05c4e6e052
KB-perByte <sagpaul@redhat.com>
2024-04-16 20:47:27 +0530

    fix config.d

--- scaffolder-backend-module-ansible/config.d.ts
+++ scaffolder-backend-module-ansible/config.d.ts
@@ -1,21 +1,22 @@
 export interface Config {
-  catalog?: {
-    providers?: {
-      ansible?: {
-        /**
-         * Url of the hub cluster developer hub instance
-         */
-        devSpacesBaseUrl: string;
-        /**
-         * base url for the creator-service
-         * @visibility secret
-         */
-        baseUrl: string;
-        /**
-         * port at which the creator-service is exposed
-         */
-        port?: number;
-      };
+  ansible?: {
+    /**
+     * Url of the hub cluster developer hub instance
+     */
+    devSpacesBaseUrl?: string;
+    /**
+     * Base url for the creator-service
+     */
+    ansibleCreatorService: {
+      /**
+       * Base url for the creator-service
+       * @visibility secret
+       */
+      baseUrl: string;
+      /**
+       * Port at which the creator-service is exposed
+       */
+      port: string;
     };
   };
 }
