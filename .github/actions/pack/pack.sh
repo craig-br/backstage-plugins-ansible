4d7cda9c247864c868fc3745bc1fd20c7a9a7e70
Sagar Paul <sagpaul@redhat.com>
2024-09-13 15:55:48 +0530

    Add echos and cleanup of source tar generation (#120)

--- .github/actions/pack/pack.sh
+++ .github/actions/pack/pack.sh
@@ -22,14 +22,27 @@ fi
 
 # Create source tar
 
-# Create a tar name it pack.tar.gz
+# Create a tarball named pack.tar.gz
+echo "Creating a tarball of the current directory as pack.tar.gz..."
 tar -czf pack.tar.gz .
-# Extract it under a directory called ansible-backstage-plugins-source-code-${GITHUB_REF##*/v}
+echo "Tarball pack.tar.gz created."
+
+# Extract the tarball into a directory called ansible-backstage-plugins-source-code-${GITHUB_REF##*/v}
+echo "Creating directory $sourcePackDir-${GITHUB_REF##*/v} and extracting pack.tar.gz into it..."
 mkdir $sourcePackDir-${GITHUB_REF##*/v} && tar -xzf pack.tar.gz -C $sourcePackDir-${GITHUB_REF##*/v}/
-# Repack the directoy with the desired name ansible-backstage-plugins-source-code-${GITHUB_REF##*/v}
-tar -czf $sourcePackDir-${GITHUB_REF##*/v}.tar.gz -C $sourcePackDir-${GITHUB_REF##*/v} .
-# Clean up
+echo "Extraction complete. Contents now in $sourcePackDir-${GITHUB_REF##*/v}."
+
+# Repack the directory with the desired name ansible-backstage-plugins-source-code-${GITHUB_REF##*/v}.tar.gz
+echo "Repacking the directory $sourcePackDir-${GITHUB_REF##*/v} into a new tarball ${sourcePackDir}-${GITHUB_REF##*/v}.tar.gz..."
+tar -czvf $sourcePackDir-${GITHUB_REF##*/v}.tar.gz -C $(dirname $sourcePackDir-${GITHUB_REF##*/v}) $(basename $sourcePackDir-${GITHUB_REF##*/v})
+echo "Tarball ${sourcePackDir}-${GITHUB_REF##*/v}.tar.gz created."
+
+# Clean up the original pack.tar.gz and the extracted directory
+echo "Cleaning up: Removing pack.tar.gz and the directory $sourcePackDir-${GITHUB_REF##*/v}..."
 rm -rf pack.tar.gz $sourcePackDir-${GITHUB_REF##*/v}
+echo "Cleanup complete."
+
+# End processing source tar
 
 # Create the pack destination directory
 mkdir -p "$packDestination"
