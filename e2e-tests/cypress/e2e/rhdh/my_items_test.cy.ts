a6dfe85329ce9847d216c3511fd5b958290122f7
Sandeep Shedmake <sshedmak@redhat.com>
2025-03-03 16:51:06 +0530

    [e2e-tests] Handle Authorize 'GitHub OAuth app' popup, when sign into RHDH using GitHub (#176)
    * [e2e-tests] Handle Authorize 'GitHub OAuth app' popup, when sign into RHDH using GitHub
    
    * Fix trailing whitespace in utils/common.ts
    
    * Removed '.contains('Authorize test-rhaap-1')'
    
    * Refactor GitHub login
    
    * Fix pre-commit failures

--- e2e-tests/cypress/e2e/rhdh/my_items_test.cy.ts
+++ e2e-tests/cypress/e2e/rhdh/my_items_test.cy.ts
@@ -2,17 +2,10 @@ import { Common } from '../utils/common';
 
 describe('Developer Hub ansible plugin tests', () => {
   before(() => {
-    Common.LogintoGithub();
+    Common.SignIntoRHDHusingGithub();
   });
 
   it('Visits My Items tab   and check all links there', () => {
-    cy.visit('/');
-    cy.wait(5000);
-    cy.contains('Sign In').invoke('removeAttr', 'target').click();
-    cy.wait(30000);
-    cy.contains('Ansible').click();
-    cy.url().should('include', '/ansible');
-    cy.wait(5000);
     cy.get('[data-testid="header-tab-1"]').click();
     cy.url().should('include', '/myitems');
     cy.wait(5000);
--- e2e-tests/cypress/e2e/rhdh/overview_test.cy.ts
+++ e2e-tests/cypress/e2e/rhdh/overview_test.cy.ts
@@ -2,7 +2,7 @@ import { Common } from '../utils/common';
 
 describe('Developer Hub ansible plugin tests', () => {
   before(() => {
-    Common.LogintoGithub();
+    Common.SignIntoRHDHusingGithub();
   });
 
   it('Visits overview tab  check all links there', () => {
@@ -13,13 +13,6 @@ describe('Developer Hub ansible plugin tests', () => {
     cy.log(devspaceurl);
     cy.log(ahuburl);
     cy.log(aucontroller);
-    cy.visit('/');
-    cy.wait(5000);
-    cy.contains('Sign In').invoke('removeAttr', 'target').click();
-    cy.wait(30000);
-    cy.contains('Ansible').click();
-    cy.url().should('include', '/ansible');
-    cy.wait(5000);
     cy.contains('Start Learning Path').click();
     cy.url().should('include', '/learn');
     cy.go('back');
