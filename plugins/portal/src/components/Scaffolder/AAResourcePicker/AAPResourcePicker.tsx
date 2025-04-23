d0f2f5c02de1fe0c06960e90022d86446d2cf59c
Ajinkya Udgirkar <ajinkyaudgirkar@gmail.com>
2025-04-23 16:35:43 +0530

    AAPResourcePicker should allow to select different options (#218)
    * Fixed issue with AAPResourcePicker which did not allow user to select different options from dropdown
    
    * Allow the multiple selected values to be removed

--- plugins/portal/src/components/Scaffolder/AAResourcePicker/AAPResourcePicker.tsx
+++ plugins/portal/src/components/Scaffolder/AAResourcePicker/AAPResourcePicker.tsx
@@ -1,5 +1,4 @@
-import React, { useCallback, useState } from 'react';
-import useDebounce from 'react-use/esm/useDebounce';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useApi } from '@backstage/core-plugin-api';
 import {
   scaffolderApiRef,
@@ -9,8 +8,33 @@ import {
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 
-import { InputLabel, MenuItem, Select } from '@material-ui/core';
+import {
+  Chip,
+  CircularProgress,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Select,
+} from '@material-ui/core';
 import { rhAapAuthApiRef } from '../../../apis';
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300,
+  },
+  chips: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  chip: {
+    margin: 2,
+  },
+  noLabel: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
 export const AAPResourcePicker = (props: ScaffolderRJSFFieldProps) => {
   const {
@@ -24,23 +48,34 @@ export const AAPResourcePicker = (props: ScaffolderRJSFFieldProps) => {
     formData,
     onChange,
   } = props;
-  const _idKey = idKey ?? 'id';
-  const _nameKey = nameKey ?? 'name';
+  const _idKey: string = idKey ?? 'id';
+  const _nameKey: string = nameKey ?? 'name';
+  const multiple = type === 'array';
+
+  const getInitValue = () => {
+    if (!formData) return [];
+    if (multiple) {
+      return formData.map((item: { [x: string]: any }) => item[_idKey]);
+    }
+    return formData[_idKey];
+  };
+
   const aapAuth = useApi(rhAapAuthApiRef);
   const scaffolderApi = useApi(scaffolderApiRef);
-  const [availableResources, setAvailableResources] = useState<{}[]>([]);
-  const multiple = type === 'array';
-  let selected: string | number | string[] | number[] | null = null;
-  if (formData) {
-    selected = multiple
-      ? // @ts-ignore
-        formData.map((item: {}) => item[_idKey])
-      : formData[_idKey];
-  }
+  const [availableResources, setAvailableResources] = useState<Array<Object>>(
+    [],
+  );
+  const [selected, setSelected] = useState<
+    string | number | string[] | number[]
+    // @ts-ignore
+  >(getInitValue);
+  const [loading, setLoading] = useState<boolean>(false);
+  const classes = useStyles();
 
   const updateAvailableResources = useCallback(() => {
     aapAuth.getAccessToken().then((token: string) => {
       if (scaffolderApi.autocomplete) {
+        setLoading(true);
         scaffolderApi
           .autocomplete({
             token: token,
@@ -49,33 +84,59 @@ export const AAPResourcePicker = (props: ScaffolderRJSFFieldProps) => {
           })
           .then(({ results }) => {
             setAvailableResources(results);
+            setLoading(false);
           })
           .catch(() => {
             setAvailableResources([]);
+            setLoading(false);
           });
       } else {
         setAvailableResources([]);
+        setLoading(false);
       }
     });
   }, [aapAuth, resource, scaffolderApi]);
-  useDebounce(updateAvailableResources, 500, [updateAvailableResources]);
+  useEffect(updateAvailableResources, [updateAvailableResources]);
 
   function change(event: any) {
-    const {
+    let {
       target: { value },
     } = event;
-    let endValue: {} | {}[] | undefined;
+    let endValue: Object | Array<Object> | undefined;
     if (multiple) {
-      endValue = availableResources.filter(item =>
-        // @ts-ignore
+      value = value.map((e: any) => (e instanceof Object ? e[_idKey] : e));
+      endValue = availableResources.filter((item: any) =>
         value.includes(item[_idKey]),
       );
     } else {
       // @ts-ignore
       endValue = availableResources.find(res => res[_idKey] === value);
     }
+    // @ts-ignore
+    setSelected(multiple ? value : endValue[_idKey]);
     onChange(endValue);
   }
+
+  const renderSelectedValues = (values: any) => {
+    let items;
+    if (typeof values[0] === 'number') {
+      items = availableResources.filter((e: any) => values.includes(e[_idKey]));
+    } else {
+      items = values;
+    }
+    return (
+      <div className={classes.chips}>
+        {/* @ts-ignore */}
+        {items.map(value => (
+          <Chip
+            key={value[_idKey]}
+            label={value[_nameKey]}
+            className={classes.chip}
+          />
+        ))}
+      </div>
+    );
+  };
 
   return (
     <FormControl
@@ -85,10 +146,10 @@ export const AAPResourcePicker = (props: ScaffolderRJSFFieldProps) => {
       disabled={disabled}
     >
       <InputLabel id={`${name}-select-label`}>
-        {title ?? 'Inventory'}
+        {title ?? 'Inventory'}&nbsp;
+        {loading && <CircularProgress size={12} />}
       </InputLabel>
       <Select
-        native={false}
         placeholder={title ?? 'Inventory'}
         multiple={multiple}
         labelId={`${name}-select-label`}
@@ -96,6 +157,9 @@ export const AAPResourcePicker = (props: ScaffolderRJSFFieldProps) => {
         // @ts-ignore
         onChange={change}
         value={selected}
+        {...(multiple && {
+          renderValue: renderSelectedValues,
+        })}
       >
         {availableResources.map((item, index) => (
           // @ts-ignore
